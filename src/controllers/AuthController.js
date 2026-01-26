32afae35a09096425294e29b7e1d6c8d1118e5bd
oliveira <oliveiranathan853@gmail.com>
2026-01-25 23:13:49 -0300

    feat: criar rota da pagina de criar conta

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,3 +1,7 @@
 export const loginPage = (req, res) => {
     res.render('auth/login')
 }
+
+export const registerPage = (req, res) => {
+    res.render('auth/register')
+}
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router()
 
-import { loginPage} from '../controllers/AuthController.js'
+import { loginPage, registerPage } from '../controllers/AuthController.js'
 
 router.get('/login', loginPage)
-
+router.get('/register', registerPage)
 
 
 export default router
